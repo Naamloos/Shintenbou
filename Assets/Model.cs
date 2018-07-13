2b119b64bcfc94ebdc351f3d36511aa25b725bdd
li223 <li223@users.noreply.github.com>
2018-07-13 19:11:39 +0100

    Moved Model.cs

--- Assets/Model.cs
+++ Assets/Model.cs
@@ -1,20 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using Shintenbou.Rest.Objects;
 
 namespace Shintenbou
 {
     public class Database : DbContext
     {
-        public DbSet<TrackingData> TrackingData { get; set; }
+        public DbSet<AnilistAnime> FavouriteAnime { get; set; }
+        //This will need to be changed to AnilistManga
+        public DbSet<object> FavouriteManaga { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=shintenbou.db");
         }
-    }
-
-    public class TrackingData
-    {
-
     }
 }
