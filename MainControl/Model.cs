5f15224ae541923403b486f07a8623e2d4341e34
li223 <li223@users.noreply.github.com>
2018-07-14 03:23:17 +0100

    Start of the Tracking Page

--- MainControl/Model.cs
+++ MainControl/Model.cs
@@ -1,18 +1,80 @@
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
-using Shintenbou.Rest.Objects;
+using System.ComponentModel;
 
 namespace Shintenbou
 {
     public class Database : DbContext
     {
-        public DbSet<AnilistAnime> FavouriteAnime { get; set; }
-        //This will need to be changed to AnilistManga
-        public DbSet<object> FavouriteManaga { get; set; }
+        /// <summary>
+        /// Contains all the user's favourited anime
+        /// </summary>
+        public DbSet<FavouritedAnime> FavouriteAnime { get; set; }
+        
+        /// <summary>
+        /// Contains all the user's favourited manga
+        /// </summary>
+        public DbSet<FavouritedManga> FavouriteManaga { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=shintenbou.db");
         }
+    }
+
+    public class FavouritedAnime
+    {
+        /// <summary>
+        /// Item ID for EF Core
+        /// </summary>
+        public int Id { get; private set; }
+
+        /// <summary>
+        /// Name of the Anime
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Anime's description
+        /// </summary>
+        public string Description { get; private set; }
+
+        /// <summary>
+        /// The cover url
+        /// </summary>
+        public string ImageUrl { get; private set; }
+
+        /// <summary>
+        /// The time at which the user stopped/paused at
+        /// </summary>
+        public float TimeIndex { get; private set; }
+    }
+
+    public class FavouritedManga
+    {
+        /// <summary>
+        /// Item ID for EF Core
+        /// </summary>
+        public int Id { get; private set; }
+
+        /// <summary>
+        /// Name of the Manga
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Manga's description
+        /// </summary>
+        public string Description { get; private set; }
+
+        /// <summary>
+        /// The cover url
+        /// </summary>
+        public string ImageUrl { get; private set; }
+
+        /// <summary>
+        /// The page the user was last on
+        /// </summary>
+        public int Page { get; private set; }
     }
 }
--- Pages/TrackingPage.xaml.cs
+++ Pages/TrackingPage.xaml.cs
@@ -1,19 +1,79 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using Avalonia.Media;
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using Shintenbou;
 
 namespace Shintenbou.Pages
 {
     public class TrackingPage : UserControl
     {
+        Database Db { get; set; }
+        Grid Grid { get; set; }
         public TrackingPage()
         {
+            Db = new Database();
             this.InitializeComponent();
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+            Grid = this.Find<Grid>("TrackingGrid");
         }
+        
+        //ef core is being weird and wont see that the tables exist so I've put this in comments to allow the app to run
+        /*
+        private void LoadFavoriteAnime()
+        {
+            for(var i = 0; i < Db.FavouriteAnime.Count(); i++) 
+            {
+                Grid.ColumnDefinitions.Add(new ColumnDefinition()
+                {
+                    MinWidth = 150
+                });
+                var child = new Image()
+                {
+                    Margin = new Thickness(50, 0, 0, 0),
+                    IsVisible = true,
+                    Name = $"Img{i}",
+                    MinWidth = 150,
+                    MinHeight = 250,
+                    Source = new Bitmap(Db.FavouriteAnime[i].CoverImage.Medium)
+                };
+                child.SetValue(Grid.ColumnProperty, i);
+                child.SetValue(Grid.RowProperty, 1);
+                Grid.Children.Add(child);
+            }
+        }
+
+        private void LoadFavoriteManga()
+        {
+            for(var i = 0; i < Db.FavouriteManga.Count(); i++) 
+            {
+                Grid.ColumnDefinitions.Add(new ColumnDefinition()
+                {
+                    MinWidth = 150
+                });
+                var child = new Image()
+                {
+                    Margin = new Thickness(50, 0, 0, 0),
+                    IsVisible = true,
+                    Name = $"Img{i}",
+                    MinWidth = 150,
+                    MinHeight = 250,
+                    Source = new Bitmap(Db.FavouriteManga[i].CoverImage.Medium)
+                };
+                child.SetValue(Grid.ColumnProperty, i);
+                child.SetValue(Grid.RowProperty, 4);
+                Grid.Children.Add(child);
+            }
+        }
+        */
     }
 }
