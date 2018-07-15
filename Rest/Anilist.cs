876928bf26c5dae9b087e76bd9fcdef8ecb55eb2
Izu <rannick10@tutanota.com>
2018-07-15 21:42:55 +0200

    here you go li, pls no hit

--- Rest/Anilist.cs
+++ Rest/Anilist.cs
@@ -1,11 +1,41 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
 using Shintenbou.Rest.Objects;
+using Newtonsoft.Json;
 
 namespace Shintenbou.Rest
 {
     public static class Anilist
     {
-        public static AnilistAnime[] GetAnimeByName(string name)
-            => throw new NotImplementedException();
+        private static HttpClient _client = new HttpClient();
+
+        public static async Task<IEnumerable<AnilistAnime>> GetAnimeByName(string name)
+        {
+            string schema = string.Empty;
+            var resourceStream = Assembly.GetEntryAssembly().GetManifestResourceStream("Shintenbou.Rest.Schema.AnilistAnime.graphql");
+
+            using (var ms = new MemoryStream())
+            {
+                resourceStream.CopyTo(ms);
+
+                using (var sr = new StreamReader(ms))
+                    schema = sr.ReadToEnd();
+            }
+            
+            var req = new AnilistRequest();
+            req.Query = schema;
+            req.Variables.Add("search", name.Replace("'", string.Empty));
+            
+            using (var reqMessage = new HttpRequestMessage())
+            {
+                reqMessage.Content = JsonConvert.SerializeObject(req);
+                reqMessage.Method = HttpMethod.Post;
+
+            }
+        }
     }
 }
